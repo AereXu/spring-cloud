a976ca96df386d1bfa127c3c475a1867947aec79
AereXu <xupeng@quyiyuan.com>
2017-03-19 17:43:33 +0800

    Update demo-service-client

---
+++ demo-service-client/src/main/java/com/aerexu/test/cloud/client/service/config/ZipkinConfig.java
@@ -0,0 +1,17 @@
+package com.aerexu.test.cloud.client.service.config;
+
+import com.aerexu.test.cloud.core.annotation.EnableRibbonZipkin;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/2/28 16:15
+ * @version: 1.0.0
+ */
+@Configuration
+@EnableRibbonZipkin
+public class ZipkinConfig {
+
+}
