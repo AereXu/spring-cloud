ae586f580c9771409084d3e642afc1cb457c7a18
AereXu <xupeng@quyiyuan.com>
2017-03-19 21:42:47 +0800

    Add Zuul router

---
+++ zuul-router/src/main/java/com/aerexu/test/cloud/server/zuul/config/FilterConfig.java
@@ -0,0 +1,20 @@
+package com.aerexu.test.cloud.server.zuul.config;
+
+import com.aerexu.test.cloud.server.zuul.filter.TestFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/2/20 19:21
+ * @version: 1.0.0
+ */
+@Configuration
+public class FilterConfig {
+    @Bean
+    public TestFilter testFilter(){
+        return new TestFilter();
+    }
+}
---
+++ zuul-router/src/main/java/com/aerexu/test/cloud/server/zuul/config/SSLConfig.java
@@ -0,0 +1,18 @@
+package com.aerexu.test.cloud.server.zuul.config;
+
+import com.aerexu.test.cloud.core.annotation.EnableEurekaSSL;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/2/16 15:07
+ * @version: 1.0.0
+ */
+@Configuration
+@EnableEurekaSSL
+public class SSLConfig {
+
+}
