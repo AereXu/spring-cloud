65deb0d64b37472e8db0b84ca03517c0ca68e101
AereXu <xupeng@quyiyuan.com>
2017-03-19 18:10:58 +0800

    Add module zipkin server

---
+++ zipkin-server/src/main/java/com/aerexu/test/cloud/server/zipkin/Application.java
@@ -0,0 +1,23 @@
+package com.aerexu.test.cloud.server.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
+import zipkin.server.brave.BraveConfiguration;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/2/27 15:30
+ * @version: 1.0.0
+ */
+@SpringBootApplication(exclude = {BraveConfiguration.class})
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ zipkin-server/src/main/java/com/aerexu/test/cloud/server/zipkin/config/SSLConfig.java
@@ -0,0 +1,17 @@
+package com.aerexu.test.cloud.server.zipkin.config;
+
+import com.aerexu.test.cloud.core.annotation.EnableEurekaSSL;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/2/16 15:07
+ * @version: 1.0.0
+ */
+@Configuration
+@EnableEurekaSSL
+public class SSLConfig {
+
+}
