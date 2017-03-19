08365abd889510a9e0e6babf7c9c3c35f399ebe5
AereXu <xupeng@quyiyuan.com>
2017-03-19 17:24:00 +0800

    Update eureka server

--- eureka-server/src/main/java/com/aerexu/test/cloud/server/eureka/Application.java
+++ eureka-server/src/main/java/com/aerexu/test/cloud/server/eureka/Application.java
@@ -1,7 +1,7 @@
-package com.aerexu.test.cloud.server;
+package com.aerexu.test.cloud.server.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -15,6 +15,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ eureka-server/src/main/java/com/aerexu/test/cloud/server/eureka/config/SSLConfig.java
@@ -0,0 +1,44 @@
+package com.aerexu.test.cloud.server.eureka.config;
+
+import com.aerexu.test.cloud.core.annotation.EnableEurekaSSL;
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.discovery.EurekaClientConfig;
+import com.netflix.eureka.EurekaServerConfig;
+import com.netflix.eureka.cluster.PeerEurekaNodes;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import com.netflix.eureka.resources.ServerCodecs;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/2/16 15:07
+ * @version: 1.0.0
+ */
+@Configuration
+@EnableEurekaSSL
+@EnableEurekaServer
+@Slf4j
+public class SSLConfig {
+
+    @Bean
+    @Primary
+    @RefreshScope
+    @Autowired
+    public PeerEurekaNodes peerEurekaNodes(PeerAwareInstanceRegistry registry,
+                                           EurekaServerConfig serverConfig,
+                                           EurekaClientConfig clientConfig,
+                                           ServerCodecs serverCodecs,
+                                           ApplicationInfoManager applicationInfoManager) {
+        return new CustomPeerEurekaNodes(registry, serverConfig,
+                clientConfig, serverCodecs, applicationInfoManager);
+    }
+
+}
