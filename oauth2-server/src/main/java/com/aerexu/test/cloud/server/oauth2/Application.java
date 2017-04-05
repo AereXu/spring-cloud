fa4d58f928869e184d7a2384553ec6a6a763e41d
AereXu <xupeng@quyiyuan.com>
2017-04-05 14:57:11 +0800

    Add oauth2 jwt server

---
+++ oauth2-server/src/main/java/com/aerexu/test/cloud/server/oauth2/Application.java
@@ -0,0 +1,19 @@
+package com.aerexu.test.cloud.server.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @task:
+ * @discrption:
+ * @author: Aere
+ * @date: 2017/3/16 11:37
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
